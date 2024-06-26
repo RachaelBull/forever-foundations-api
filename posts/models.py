6ec1fbe6b164170ca987d64d3280699b949777b4
Racahel Bull <rachaelbull2021@gmail.com>
2024-06-26 16:20:54 +0000

    Create Post model and PostSerializer

---
+++ posts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+LOCATION_OPTIONS = (
+    ('greece', 'GREECE'),
+    ('spain', 'SPAIN'),
+    ('italy', 'ITALY'),
+    ('england', 'ENGLAND'),
+)
+
+THEME_OPTIONS = (
+    ('beach', 'BEACH'),
+    ('barn', 'BARN'),
+    ('classic', 'CLASSIC'),
+    ('rustic', 'RUSTIC'),
+    ('fairytale', 'FAIRYTALE'),
+)
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    location = models.TextField(max_length=7, choices=LOCATION_OPTIONS, default='england')
+    theme = models.CharField(max_length=9, choices=THEME_OPTIONS, default='classic')
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_a0da7q', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
