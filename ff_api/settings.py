65c9a8d5095ce578d47ec09c7166d62df995f08f
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-25 09:42:10 +0000

    Add a new client origin

--- ff_api/settings.py
+++ ff_api/settings.py
@@ -113,7 +113,8 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN_DEV')
+         os.environ.get('CLIENT_ORIGIN_DEV'),
+         os.environ.get('CLIENT_ORIGIN')
     ]
 
 CORS_ALLOW_CREDENTIALS = True
