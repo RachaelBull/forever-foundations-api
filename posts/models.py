3c491c9debb56dbfe0ff6195f3d6022fa7af595f
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-30 15:16:18 +0000

    Alter location and theme post model fields

--- posts/models.py
+++ posts/models.py
@@ -27,8 +27,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    location = models.TextField(max_length=7, choices=LOCATION_OPTIONS, default='england')
-    theme = models.CharField(max_length=9, choices=THEME_OPTIONS, default='classic')
+    location = models.TextField(max_length=7, choices=LOCATION_OPTIONS, default='Not Specified')
+    theme = models.CharField(max_length=9, choices=THEME_OPTIONS, default='Not Specified')
     image = models.ImageField(
         upload_to='images/', default='../default_post_a0da7q', blank=True
     )
