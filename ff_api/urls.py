2f81960b824fc31c4dde997f3c74189389763869
Racahel Bull <rachaelbull2021@gmail.com>
2024-08-06 22:18:24 +0000

    Add reviews url to the main api urls

--- ff_api/urls.py
+++ ff_api/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     ),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
+    path('', include('reviews.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
