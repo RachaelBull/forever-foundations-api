87e1b14457c7d23215643e599ab5c7faec8bcf64
Racahel Bull <rachaelbull2021@gmail.com>
2024-08-09 11:39:57 +0000

    Change serializer field names

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,11 +3,11 @@ from rest_framework import serializers
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner_username = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    created_on = serializers.SerializerMethodField()
+    natural_created_on = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
